f64e6abd4b3dc297cea78d738e2fd8978b2eb409
Omayma El Hadari <om.elhadari@gmail.com>
2024-02-09 22:48:07 +0100

    code alongs dia 4 de react

---
+++ module-3/react-lifting-state-up/src/components/Summary.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+// src/components/Summary.jsx
+
+import React from "react";
+
+// src/components/Summary.jsx
+
+function Summary(props) {
+    return (
+      <div>
+        <h1>TASKS COMPLETED:</h1>
+        <p className="tasks-completed">{props.tasksCompleted}</p>     {/*   UPDATE   */}
+      </div>
+    );
+  }
+  
+  export default Summary;
+  
---
+++ module-3/react-lifting-state-up/src/components/Task.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+// src/components/Task.jsx
+
+function Task(props) {
+  
+    return (
+      <div className="task-card">
+        <div className="task-card-half">
+          <h1>{props.task.name}</h1>
+          {props.task.isDone
+            ? <span>DONE </span>
+            : <span>PENDING ⌛</span>
+          }
+  
+          <h2> Task Description </h2>
+          <p>{props.task.description}</p>
+  
+          <button 
+            className="add"
+            onClick={() => props.toggleTask(props.task._id)}       // <== UPDATE 
+          >
+            {props.task.isDone 
+              ? <span>UNDO </span> 
+              : <span>✔️</span>
+            }
+          </button>
+  
+        </div>
+      </div>
+    );
+  }
+  
+  export default Task;
+  
